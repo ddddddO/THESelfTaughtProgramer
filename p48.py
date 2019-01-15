23823f7a37103e6d1b9a2c420fa1cffe1ee33ad5
dddddO <lbfdeatq@gmail.com>
2019-01-15 23:18:16 +0900

    p48

---
+++ p48.py
@@ -0,0 +1,36 @@
+# p48 チャレンジ
+# 1
+print("a")
+print("b")
+print("vvv")
+
+#2
+val1 = 4
+if val1 < 10:
+    print("val1 < 10")
+else:
+    print("val1 >= 10")
+
+#3
+val2 = 26
+if val2 <= 10:
+    print("val2 <= 10")
+elif val2 <= 25:
+    print("10 < val2 <= 25")
+else:
+    print("val2 > 25")
+
+#4
+print(11%5)
+
+#5
+print(17//3)
+
+#6
+age = 26
+if age <= 3:
+    print("milke!")
+elif age < 20:
+    print("juice!!")
+else:
+    print("alcohol!!!")
